e2080e97517ed28c55b92bba3d5a70fba1bef3f5
Will Gresham <william.gresham@mindera.com>
2020-12-03 00:49:38 +0000

    Initial commit including solution to day 1

---
+++ src/day-01/index.ts
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const numbers = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8').split('\n').map(number => +number).filter(number => number > 0)
+
+const partOne = () => {
+  numbers.some(number => {
+    const index = numbers.indexOf(2020 - number)
+    if (index > -1) {
+      console.log(`Part 1: ${number * numbers[index]}`)
+      return true
+    }
+  })
+}
+
+const partTwo = () => {
+  numbers.some(numberA => numbers.some(numberB => {
+    const index = numbers.indexOf(2020 - numberA - numberB)
+    if (index > -1) {
+      console.log(`Part 2: ${numberA * numberB * numbers[index]}`)
+      return true
+    }
+  }))
+}
+
+partOne()
+partTwo()
