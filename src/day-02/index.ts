71a33ddf5330f2c5df9fb2c3b2f419b169a634a7
Will Gresham <will@willgresham.me>
2020-12-04 04:16:10 +0000

    Refactor with part 2 in mind

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -1,18 +1,17 @@
 import getInput from '../get-input'
 
 const data = getInput().split('\n').filter(item => item).map(item => {
-  const [rules, password] = item.split(':')
-  const [restriction, character] = rules.split(' ')
-  const [min, max] = restriction.split('-')
+  const [restrictions, character, password] = item.split(' ')
+  const [restrictionA, restrictionB] = restrictions.split('-')
   return {
-    password: password.trim(), character, min, max
+    password, character: character.replace(':', ''), restrictionA, restrictionB
   }
 })
 
 const partOne = () => {
   console.log(data.filter(item => {
     const matches = (item.password.match(new RegExp(item.character, 'g')) || []).length
-    return matches >= +item.min && matches <= +item.max
+    return matches >= +item.restrictionA && matches <= +item.restrictionB
   }).length)
 }
 
